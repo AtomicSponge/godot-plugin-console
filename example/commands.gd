39d24e9541fbc1c1aac4345a336a1720528e347b
Matthew Evans <mevans+git@wtfsystems.net>
2025-08-19 20:24:58 -0400

    sync

--- example/commands.gd
+++ example/commands.gd
@@ -1,4 +1,13 @@
 extends Node
 
+func say_command(text: String) -> void:
+	Console.add_text(text)
+
+func quit_command(_arg: String) -> void:
+	get_tree().quit()
+
 func _ready() -> void:
-	pass
+	Console.set_console_size(Vector2(550, 200))
+	Console.set_position(Vector2(2, 410))
+	Console.add_command("say", say_command)
+	Console.add_command("quit", quit_command)
